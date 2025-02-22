ace11af46d21c96c641641bee8b1ff71fa66743d
Senujan18 <89368473+Senujan18@users.noreply.github.com>
2025-02-22 20:25:05 +1300

    changed online ordering

--- js/order.js
+++ js/order.js
@@ -1,5 +1,5 @@
 const order_btn = document.getElementById('order_button');
 
 order_btn.addEventListener('click', ()=> {
-    window.location.href="https://www.ordermeal.co.nz/kothu-kadai/";
+    window.location.href="https://kothukadai.booknorder.co.nz";
 })
